5e10abfd783c1b33af24316ce42ddf51d5dd245c
AramWondergem <aramwondergem@gmail.com>
2024-08-15 17:09:56 -0400

    finished structure of api

---
+++ src/app/api/business-groups/route.ts
@@ -0,0 +1,18 @@
+import {FilterBody} from "@/interfaces/body/filterBody";
+
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+export const runtime = 'nodejs';
+
+
+// based on the selected filters, the amount of resources per types of disposal are send back differentiated by business group
+export async function POST(request: Request) {
+    try {
+        const body: FilterBody = await request.json() as FilterBody;
+
+        return new Response(JSON.stringify(body));
+
+    } catch (error) {
+        return new Response('Invalid request body', {status: 400});
+    }
+
+}
--- src/app/api/counties/totals/route.ts
+++ src/app/api/counties/totals/route.ts
@@ -3,12 +3,13 @@ import {FilterBody} from "@/interfaces/body/filterBody";
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 export const runtime = 'nodejs';
 
+
+// based on the selected filters, the totals of resources per county are returned
 export async function POST(request: Request) {
     try {
+        const body: FilterBody = await request.json() as FilterBody;
 
-        const body: FilterBody = request.json() as FilterBody;
-
-        return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+        return new Response(JSON.stringify(body));
 
     } catch (error) {
         return new Response('Invalid request body', {status: 400});
