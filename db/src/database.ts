38507f8cafa67cfaabb833c6609ce8d72eb11c4f
Bonnie Nortz <bonnie.rose.nortz@gmail.com>
2024-08-28 16:29:42 -0400

    Remove extraneous whitespace from database.ts

--- db/src/database.ts
+++ db/src/database.ts
@@ -3,10 +3,6 @@ import { createKysely } from '@vercel/postgres-kysely'
 
 require('dotenv').config({path: '.env.local'});
 
-// console.log({POSTGRES_URL: process.env.POSTGRES_URL})
-
-
-
 export const db = createKysely<Database>({
 
   connectionString: process.env.POSTGRES_URL,
