7f97b2557e14a7242cc9c7bb1fdc2318c6cce3af
Bonnie Nortz <bonnie.rose.nortz@gmail.com>
2024-08-28 16:28:37 -0400

    Add comments to JurisdictionRepository functions
    To parallel other similar files

--- db/src/JurisdictionRepository.ts
+++ db/src/JurisdictionRepository.ts
@@ -8,6 +8,7 @@ export async function findJurisdictionById(jurisdiction_id: number) {
     .executeTakeFirst()
 }
 
+// Find a jurisdiction by its ID
 export async function findJurisdictions(criteria: Partial<Jurisdiction>) {
   let query = db.selectFrom('jurisdiction')
 
@@ -34,10 +35,12 @@ export async function findJurisdictions(criteria: Partial<Jurisdiction>) {
   return await query.selectAll().execute()
 }
 
+// Update a jurisdiction by its ID
 export async function updateJurisdiction(jurisdiction_id: number, updateWith: JurisdictionUpdate) {
   await db.updateTable('jurisdiction').set(updateWith).where('jurisdiction_id', '=', jurisdiction_id).execute()
 }
 
+// Create a new jurisdiction entry
 export async function createJurisdiction(jurisdiction: NewJurisdiction) {
   return await db.insertInto('jurisdiction')
     .values(jurisdiction)
@@ -45,6 +48,7 @@ export async function createJurisdiction(jurisdiction: NewJurisdiction) {
     .executeTakeFirstOrThrow()
 }
 
+// Delete a jurisdiction by its ID
 export async function deleteJurisdiction(jurisdiction_id: number) {
   return await db.deleteFrom('jurisdiction').where('jurisdiction_id', '=', jurisdiction_id)
     .returningAll()
